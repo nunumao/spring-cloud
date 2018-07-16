66eea63789235ff408079203ac3fee9a3ce949a3
朱荻 <292018748@qq.com>
2018-07-17 01:09:37 +0800

    2018-07-17

---
+++ service-feign/src/main/java/com/a365d1/servicefeign/web/HiContorller.java
@@ -0,0 +1,22 @@
+package com.a365d1.servicefeign.web;
+
+
+import com.a365d1.servicefeign.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiContorller {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+
+}
